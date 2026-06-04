fbc816e8914a1ae4a5223a381f6cd5e233a8f1a9
Narndra varma Koneti <konetinarendravarma212@gmail.com>
2026-06-04 17:37:01 +0530

    new changes

--- Java Script 3/js.js
+++ Java Script 3/js.js
@@ -1,8 +1,45 @@
 
-var deposit = prompt("Enter weight in pounds")
+// var deposit = prompt("Enter weight in pounds")
 
-var weight = deposit * 0.454
+// var weight = deposit * 0.454
 
-alert("your weight in kg is: " +weight)
+// alert("your weight in kg is: " +weight)
 
-console.log("Conversion Completed")
+// console.log("Conversion Completed")
+
+// var hot = false
+
+// var temp = 10
+
+// if(temp > 80){
+//     console.log("hot outside: ")
+//     hot = true
+// }
+// else if (temp <= 80 && temp >= 50){
+//     console.log("normal")
+// }
+// else if (temp < 50 && temp >= 20){
+//     console.log("its cold")
+// }
+// else{
+//     console.log("Super cold outside")
+// }
+// console.log(hot)
+
+
+var ham = 10 
+
+var cheese = 6
+
+var report = "Blank"
+
+if(ham >= 10 && cheese >= 9){
+    report = console.log("good sales ")
+}
+else if (ham == 0 && cheese == 0){
+    console.log("nothing sold")
+}
+else{
+    console.log("Some items sold")
+}
+console.log(report)
