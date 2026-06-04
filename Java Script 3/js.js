407104da57aa807f6976ab3de1273b9acbe89e07
Narndra varma Koneti <konetinarendravarma212@gmail.com>
2026-06-04 20:08:10 +0530

    javascript part four and five

--- Java Script 3/js.js
+++ Java Script 3/js.js
@@ -27,19 +27,54 @@
 // console.log(hot)
 
 
-var ham = 10 
+// var ham = 10 
 
-var cheese = 6
+// var cheese = 6
 
-var report = "Blank"
+// var report = "Blank"
 
-if(ham >= 10 && cheese >= 9){
-    report = console.log("good sales ")
-}
-else if (ham == 0 && cheese == 0){
-    console.log("nothing sold")
-}
-else{
-    console.log("Some items sold")
-}
-console.log(report)
+// if(ham >= 10 && cheese >= 9){
+//     report = console.log("good sales ")
+// }
+// else if (ham == 0 && cheese == 0){
+//     console.log("nothing sold")
+// }
+// else{
+//     console.log("Some items sold")
+// }
+// console.log(report)
+
+//  while loops
+
+// var x = 0;
+
+// while (x < 5){
+//     console.log("x is currently: " +x);
+//     if (x == 3){
+//         console.log("x is three ");
+//         break;
+//     }
+//     console.log("x is less than 5, adding 1 to " +x);
+//     x = x + 1;
+//     if (x >= 5){
+//         console.log("x reached it's maximum number ");
+//     }
+// }
+
+// var x = 0 
+
+// while (x <= 10){
+//     if (x % 2 === 0){
+//         console.log("even Numbers: " +x)
+//     }
+//     x += 1
+// }
+
+// var num = 0
+
+// while (num <= 10){
+//     console.log(num)
+//     num = num + 1
+// }
+
+
